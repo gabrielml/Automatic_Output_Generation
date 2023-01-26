0488bb9dbcf78582bd5d6f40cf90b1c9b2ee72c2
gml <gabrielmljob@gmail.com>
2023-01-26 17:20:38 +0100

    feat: Create a PDF report and send it by email
    First section of the assessment completed

--- scripts/emails.py
+++ scripts/emails.py
@@ -5,6 +5,7 @@ import mimetypes
 import os.path
 import smtplib
 
+
 def generate(sender, recipient, subject, body, attachment_path):
   """Creates an email with an attachement."""
   # Basic Email formatting
@@ -27,8 +28,12 @@ def generate(sender, recipient, subject, body, attachment_path):
 
   return message
 
-def send(message):
+def send(smtp_server, sender, sender_pass, message):
   """Sends the message to the configured SMTP server."""
-  mail_server = smtplib.SMTP('localhost')
+#  mail_server = smtplib.SMTP('localhost')
+  mail_server = smtplib.SMTP_SSL(smtp_server) # Connect to SMTP Server
+  mail_server.set_debuglevel(1) # FIXME: Activate Debug Mode in Object (Only for Testing purpouses!)
+
+  mail_server.login(sender, sender_pass)
   mail_server.send_message(message)
   mail_server.quit()
--- scripts/example.py
+++ scripts/example.py
@@ -3,6 +3,9 @@
 import emails
 import os
 import reports
+import getpass # Prompt the user for a password without echoing
+
+attachment_path = input("\nPlease enter the attachment path (e.g. /path/to/attachment/report.pdf)\n")
 
 table_data=[
   ['Name', 'Amount', 'Value'],
@@ -12,13 +15,21 @@ table_data=[
   ['durians', 1, 25],
   ['bananas', 5, 1.99],
   ['cherries', 23, 5.80],
-  ['grapes', 13, 2.48]]
-reports.generate("/tmp/report.pdf", "A Complete Inventory of My Fruit", "This is all my fruit.", table_data)
+  ['grapes', 13, 2.48],
+  ['kiwi', 4, 0.49]]
 
-sender = "sender@example.com"
-receiver = "{}@example.com".format(os.environ.get('USER'))
+reports.generate( attachment_path, "A Complete Inventory of My Fruit", "This is all my fruit.", table_data)
+
+# Gets the information needed to send the message
+smtp_server = input("Please insert a SMTP Server. e.g.(smtp.gmail.com)\n")
+# sender = "sender@example.com"
+sender = input("Please enter a valid sender's email address!\n")
+mail_pass = getpass.getpass("Please enter your password! (NB: in Gmail use App passwords)\n")
+#receiver = "{}@example.com".format(os.environ.get('USER'))
+receiver = input("Please enter a valid receiver's address!\n")
+
 subject = "List of Fruits"
 body = "Hi\n\nI'm sending an attachment with all my fruit."
 
-message = emails.generate(sender, receiver, subject, body, "/tmp/report.pdf")
-emails.send(message)
+message = emails.generate(sender, receiver, subject, body, attachment_path)
+emails.send(smtp_server, sender, mail_pass, message)
