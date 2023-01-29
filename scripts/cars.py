aaf155bee2747a8ffccc6051313d4baae5b0c00e
gml <gabrielmljob@gmail.com>
2023-01-29 12:13:11 +0100

    feat: Add best_year function

--- scripts/cars.py
+++ scripts/cars.py
@@ -3,6 +3,9 @@
 import json
 import locale
 import sys
+import collections
+import functools
+import operator
 
 
 def load_data(filename):
@@ -27,8 +30,9 @@ def process_data(data):
 
   most_revenue = max_revenue(data)
   most_sales = best_seller_car(data)
+  most_popular_year = best_year(data)
 
-  full_summary.extend([most_revenue, most_sales])
+  full_summary.extend([most_revenue, most_sales, most_popular_year])
 
   # max_revenue = {"revenue": 0}
   # for item in data:
@@ -80,6 +84,36 @@ def best_seller_car(data):
 	]
 
 	return summary
+
+def best_year(raw_data):
+  """3. Calculate the best sales year for all car models together"""
+  # Sales by year for every single car.
+  # E.g. <Acura>{'2004': 100}, <Honda>{'2004': 50}
+  filtered_data = list(map(selected_values, raw_data))
+
+  # Total sales by year
+  # E.g. <Acura + Honda> {'2004': 150}
+  sales_by_year = dict(functools.reduce(operator.add, map(collections.Counter, filtered_data)))
+
+  # Years sorted by sales in descending order
+  desc_sales = dict(sorted(sales_by_year.items(), key=operator.itemgetter(1), reverse=True))
+
+  # Recover the Year with the best car sales
+  best_sales_year = list(desc_sales.items())[0] # Complexity would be O(n)[O(ok)]
+
+  summary = [
+    "The most popular year was {} with {} sales.".format(best_sales_year[0], best_sales_year[1])
+  ]
+  return summary
+
+
+def selected_values(item):
+  """3.1. Returns a dictionary with the total of all cars sold in a given year"""
+  key = str(item["car"]["car_year"])
+  value = item["total_sales"]
+  new_item = { key : value }
+
+  return new_item
 
 
 def cars_dict_to_table(car_data):
