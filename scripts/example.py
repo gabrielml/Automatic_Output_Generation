50687e07ddc47b6afb94d8575c2b6e287bcf3f49
gml <gabrielmljob@gmail.com>
2023-01-29 18:26:00 +0100

    fix: Correct path
    Correct path to the folder where the reports must be stored

--- scripts/example.py
+++ scripts/example.py
@@ -13,12 +13,13 @@ table_data=[
   ['bananas', 5, 1.99],
   ['cherries', 23, 5.80],
   ['grapes', 13, 2.48]]
-reports.generate("/tmp/report.pdf", "A Complete Inventory of My Fruit", "This is all my fruit.", table_data)
+
+reports.generate("tmp/report.pdf", "A Complete Inventory of My Fruit", "This is all my fruit.", table_data)
 
 sender = "sender@example.com"
 receiver = "{}@example.com".format(os.environ.get('USER'))
 subject = "List of Fruits"
 body = "Hi\n\nI'm sending an attachment with all my fruit."
 
-message = emails.generate(sender, receiver, subject, body, "/tmp/report.pdf")
+message = emails.generate(sender, receiver, subject, body, "tmp/report.pdf")
 emails.send(message)
